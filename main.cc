5746bd15f795fee7bf649e1109aa568e3ec39912
Yamunan <yamunan.bitset@gmail.com>
2022-08-09 17:37:21 +0200

    draw projectiles

--- main.cc
+++ main.cc
@@ -1,37 +1,20 @@
 #include <raylib.h>
+#include <array>
 #include <vector>
 #include "square.h"
 
 namespace GFX
 {
 	static const unsigned w = 800, h = 800;
-	std::vector<Vector2[4]> tiles;
+	std::vector<std::array<int, 2>> tiles;
 	void Init()
 	{
 		InitWindow(w, h, "Arkanoid");
 		SetTargetFPS(60);
 		
-		Rectangle tmp;
-		for (unsigned j = 0; j < 3; j++)
-			for (unsigned i = 0; i < 4; i++)
-			{
-				/*
-				Vector2 vertices[] =
-				{
-					Vector2 { float(i) * 150 + 25, float(j) * 150 + 25 },
-					Vector2 { vertices[0] + 150, 100},
-					Vector2 { 100, 100 },
-					Vector2 { 100, 100 },
-				};*/
-				Vector2 vertices[4];
-				float x = float(i) * 150 + 25;
-				float y = float(j) * 150 + 25;
-				vertices[0] = { x, y };
-				vertices[1] = { x + 150, y };
-				vertices[2] = { x, y + 150 };
-				vertices[3] = { x + 150, y };
-				tiles.push_back(vertices);
-			}
+		for (int j = 0; j < 3; j++)
+			for (int i = 0; i < 5; i++)
+				tiles.push_back(std::array<int, 2> { i * 150 + 40, j * 150 + 40 });
 	}
 	void Update()
 	{
@@ -41,11 +24,8 @@ namespace GFX
 	{
 		ClearBackground({ 128, 128, 128, 255 });
 		DrawCircle(w / 2, h - 100, 20, GREEN);
-		for (Vector2* i : tiles)
-		{
-			//DrawPolyLinesEx(i, );
-			// TODO: Find ways to draw vertices into polygons (in Raylib v2.0, DrawPolyEx)
-		}
+		for (std::array<int, 2> i : tiles)
+			DrawRectangle(i[0], i[1], 80, 80, RED);
 	}
 	bool Open()
 	{
