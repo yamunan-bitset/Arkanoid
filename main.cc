24fcaf3cb14bf93a0c5a649f60bc9dab31fcbe61
Yamunan <yamunan.bitset@gmail.com>
2022-08-09 19:03:44 +0200

    movement of slate

--- main.cc
+++ main.cc
@@ -1,12 +1,12 @@
 #include <raylib.h>
 #include <array>
 #include <vector>
-#include "square.h"
 
 namespace GFX
 {
 	static const unsigned w = 800, h = 800;
 	std::vector<std::array<int, 2>> tiles;
+	int xpos = w / 2 - 50;
 	void Init()
 	{
 		InitWindow(w, h, "Arkanoid");
@@ -18,7 +18,16 @@ namespace GFX
 	}
 	void Update()
 	{
-		
+		if (IsKeyPressed(KEY_SPACE))
+		{
+			// TODO: ball.move = true;
+		}
+		if (IsKeyDown(KEY_LEFT))
+			xpos -= 5;
+		if (IsKeyDown(KEY_RIGHT))
+			xpos += 5;
+		if (xpos < 0) xpos = 0;
+		if (xpos + 30 > h) xpos = h - 100;
 	}
 	void Draw()
 	{
@@ -26,6 +35,7 @@ namespace GFX
 		DrawCircle(w / 2, h - 100, 20, GREEN);
 		for (std::array<int, 2> i : tiles)
 			DrawRectangle(i[0], i[1], 80, 80, RED);
+		DrawRectangle(xpos, h - 60 - 15, 100, 30, BLUE);
 	}
 	bool Open()
 	{
--- square.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-class Square
-{
-public:
-	int x, y;
-	Square(int _x, int _y) : x(_x), y(_y) { }
-	~Square();
-};
